b4383dae4f4a1a6a75b3677ed44f9711258e3843
jahoney <37678277+jahoney@users.noreply.github.com>
2018-03-26 21:20:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ restService.post("/initiative", function(req, res) {
     req.body.result.parameters.addPlayerCharacter ? req.body.result.parameters.addPlayerCharacter
       : "";
   var intentName = req.body.intentName;
-  console.log(intentName);
+  console.log("Intent Name is " + intentName.toString());
   var action = req.body.result &&
     req.body.result.parameters &&
     req.body.result.parameters.initiativeAction ? req.body.result.parameters.initiativeAction
